598b05eb04afd7361a403df536b1accce00270b2
ivanheral <ivanhernandezal@gmail.com>
2020-09-07 18:32:58 +0200

    first commit

---
+++ src/js/app.jsx
@@ -0,0 +1,8 @@
+import { ReactDOM, React } from 'react';
+import TextArea from './textarea.jsx';
+import './test.css';
+
+ReactDOM.render(
+  <TextArea />,
+  document.getElementById('app')
+);
---
+++ src/js/textarea.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+// css
+import "./test.css";
+// json
+import conf from "./test.json";
+
+const TextArea = () => {
+  const [text, setText] = useState(conf.text);
+  const [stupid, setStupid] = useState(0);
+
+  const updateState = (value) => {
+    var regex = new RegExp("(stupid)", "gi");
+    var match = text.match(regex);
+    setText(value);
+    setStupid(match?.length);
+  };
+
+  return (
+    <div>
+      <p>How many times have you written "stupid"?</p>
+      <textarea
+        class="textarea"
+        onChange={(e) => {
+          updateState(e.target.value);
+        }}
+      ></textarea>
+      times: {stupid}
+    </div>
+  );
+};
+export default TextArea;
